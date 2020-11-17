f903b18bed2bec0705a8afd46aaf19e5d7241a1c
John Kondwani Beni <johnbeni93@gmail.com>
2020-11-16 17:56:21 -0800

    Beta-2. Code runs fine and route is implemented with addition of exact path for react to control the website page indexes.

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage";
 
 export default class App extends Component {
     constructor(props) {
@@ -7,9 +8,13 @@ export default class App extends Component {
     }
 
     render() {
-    return <h1>{this.props.name}</h1>;
+        return (
+            <div>
+                <HomePage />
+            </div>
+        );
     }
 }
 
 const appDiv = document.getElementById("app");
-render(<App name="tim"/>, appDiv);
+render(<App />, appDiv);
